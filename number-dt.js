47b260f6a27729af05a80dd4305c62cb11384c1e
uitricks <learn.uitricks@gmail.com>
2023-08-02 21:50:44 +0530

    Number  Data Type

---
+++ number-dt.js
@@ -0,0 +1,11 @@
+//example of integers.
+const age = 30;
+
+const quantity = -5;
+
+const population = 789456123;
+
+// Examples of floating-point numbers
+
+const pi = 3.14159;
+const temperature = -15.5;
