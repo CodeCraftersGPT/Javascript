d773e1a3752e333a2186e7b4633bdd92e89da798
uitricks <learn.uitricks@gmail.com>
2023-08-03 10:18:45 +0530

    undefined and null data types

---
+++ undefined_null-dt.js
@@ -0,0 +1,28 @@
+// Example 1: Variable declared but not assigned a value
+let name;
+console.log(name);//undefined.
+
+// Example 2: Accessing a non-existent property or variable
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+  console.log(person.firstName);
+  console.log(person.age);//undefined
+
+// null examples.
+
+let score = 100;
+score = null;
+console.log(score); // Output: null
+
+
+
+
+
+
+
+
+
+
+
