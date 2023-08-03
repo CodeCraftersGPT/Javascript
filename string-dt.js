c34436ffb6f03e10c9a87feae8b0353711d87345
uitricks <learn.uitricks@gmail.com>
2023-08-03 08:52:34 +0530

    string-dt

---
+++ string-dt.js
@@ -0,0 +1,13 @@
+// Examples of Boolean values
+
+const isReady = true;
+const isHidden = false;
+const hasPermission = true;
+
+//Conditional statements.
+
+if (isReady) {
+    console.log("The system is ready.");
+  } else {
+    console.log("The system is not ready.");
+  }
