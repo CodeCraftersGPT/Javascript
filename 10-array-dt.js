780e130442c3267f8e896cce730febadbdac08cd
uitricks <learn.uitricks@gmail.com>
2023-08-04 21:26:09 +0530

    arrays

---
+++ 10-array-dt.js
@@ -0,0 +1,21 @@
+//array example
+
+const numbers = [1,2,3,4,5];
+// console.log(numbers[0]);
+// console.log(numbers[2]);
+
+// Example 2: Creating an array of strings
+const fruits = ["apple", "banana", "orange", "grape"];
+//console.log(fruits[1]); // Output: "banana"
+//console.log(fruits[3]); // Output: "grape"
+// set the values of an array
+// fruits[3] = 'Kiwi';
+// console.log(fruits[3]);
+
+
+// loop through array
+
+for(let i=0; i < fruits.length; i++){
+    console.log(i);
+    console.log(fruits[i]);
+}
