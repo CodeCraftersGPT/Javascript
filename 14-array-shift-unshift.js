19da1fdef15464b069cb4d71ecf9a003dead810f
uitricks <learn.uitricks@gmail.com>
2023-08-05 16:00:51 +0530

    arrayshiftandunshift

---
+++ 14-array-shift-unshift.js
@@ -0,0 +1,14 @@
+//array  unshift and shift method.
+
+
+//shift method
+
+const colors = ["red", "green", "blue"];
+
+console.log(colors);
+const shiftedColor = colors.shift();
+
+console.log(colors);
+
+
+
