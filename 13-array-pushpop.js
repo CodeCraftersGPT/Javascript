7361a10b01af430819725229a59402af92985770
uitricks <learn.uitricks@gmail.com>
2023-08-05 13:36:58 +0530

    array push and pop

---
+++ 13-array-pushpop.js
@@ -0,0 +1,19 @@
+// array push and pop methods
+
+const fruits = ["apple", "banana", "orange"];
+
+
+console.log(fruits);
+console.log(fruits.length);
+
+const removedItem = fruits.pop();
+
+console.log(removedItem);
+console.log(fruits);
+console.log(fruits.length);
+
+
+
+
+
+
