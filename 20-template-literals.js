50a99b56ea027e73d81c85a9c3e120a69e4ac225
uitricks <learn.uitricks@gmail.com>
2023-08-09 06:54:28 +0530

    template literals

---
+++ 20-template-literals.js
@@ -0,0 +1,33 @@
+//Basic Template Literal:
+
+const name = "Alice";
+const greeting = `Hello, ${name}!`;
+console.log(greeting); // Output: "Hello, Alice!"
+
+//Multiline Text:
+
+const message = `
+  This is a multiline
+  text using template literals.
+  It maintains formatting.
+`;
+console.log(message);
+/* Output:
+   This is a multiline
+   text using template literals.
+   It maintains formatting.
+*/
+
+//Expression Evaluation:
+
+const price = 10;
+const quantity = 5;
+const total = `Total cost: $${price * quantity}`;
+console.log(total); // Output: "Total cost: $50"
+
+
+
+
+
+
+
