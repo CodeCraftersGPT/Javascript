47991b24d5025888ad1f707615c3c057e11dc54a
uitricks <learn.uitricks@gmail.com>
2023-08-10 20:21:04 +0530

    array-foreach

---
+++ 23-array-foreach.js
@@ -0,0 +1,11 @@
+//array foreach method.
+
+const numbers = [1, 2, 3, 4];
+
+numbers.forEach(function(number,index,array){
+    console.log(number);
+    console.log(index);
+    console.log(array);
+}); // anonymous function.
+
+
